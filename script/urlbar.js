c7a9819a6dddd3f38dbf222cc880a22cf391ff6a
Mithgol the Webmaster <getgit@mithgol.ru>
2013-12-07 16:47:06 +0400

    add ids to urlbar elements

--- script/urlbar.js
+++ script/urlbar.js
@@ -2,4 +2,20 @@ urlbar = function(){
    if (!(this instanceof urlbar)) return new urlbar();
 
    this.history = [];
+   this.curr = -1;
+};
+
+urlbar.prototype.open = function(URL){
+   if(this.curr < this.history.length-1 ){
+      this.history.splice(this.curr + 1, this.history.length - 1 - this.curr);
+   }
+   this.history.push(URL);
+   this.curr++;
+
+   $urlbar = $('#urlbar');
+   $urlbar.find('#URL').val( URL );
+   $urlbar.find('#forward').attr('disabled', 'disabled');
+   if( this.curr > 0 ) $urlbar.find('#back').attr('disabled', '');
+
+   // TODO: open the designated content
 };
