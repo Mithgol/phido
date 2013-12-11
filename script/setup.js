3c9b0ba492dd7c9a2ba142323f50562f8d9bca97
Mithgol the Webmaster <getgit@mithgol.ru>
2013-12-11 10:48:30 +0400

    settings' processor in a separate file: script/setup.js

---
+++ script/setup.js
@@ -0,0 +1,31 @@
+setup = {};
+
+initSetup = function(){
+
+var simteconf = require('simteconf');
+var phiConf = simteconf('phido.conf', {
+   skipNames: ['//', '#']
+});
+// Read GoldED settings:
+var encodingGED = phiConf.last('EncodingGoldED') || 'utf8';
+var confGED = simteconf(phiConf.last('ConfigGoldED'), {
+   encoding: encodingGED,
+   skipNames: ['//', '#', '-'+'-']
+});
+setup.UserName = phiConf.first('UserName') || confGED.first('UserName') || 'anonymous';
+// Read HPT areas:
+var encodingHPT = phiConf.last('EncodingHPT') || 'utf8';
+setup.areas = simteconf(phiConf.last('AreasHPT'), {
+   encoding: encodingHPT,
+   skipNames: ['#'],
+   lowercase: false,
+   prefixGroups: [
+      'NetmailArea',
+      'BadArea',
+      'DupeArea',
+      'LocalArea',
+      'EchoArea'
+   ]
+});
+
+};
