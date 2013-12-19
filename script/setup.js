11efd96c06c3fba5e7aaff77540e9cd82b120e62
Mithgol the Webmaster <getgit@mithgol.ru>
2013-12-19 09:22:29 +0400

    changed indent in setup.js to make JSHint happy

--- script/setup.js
+++ script/setup.js
@@ -1,31 +1,34 @@
+/* global setup:true, initSetup:true */
+
 setup = {};
 
 initSetup = function(){
 
-var simteconf = require('simteconf');
-var phiConf = simteconf('phido.conf', {
-   skipNames: ['//', '#']
-});
-// Read GoldED settings:
-var encodingGED = phiConf.last('EncodingGoldED') || 'utf8';
-var confGED = simteconf(phiConf.last('ConfigGoldED'), {
-   encoding: encodingGED,
-   skipNames: ['//', '#', '-'+'-']
-});
-setup.UserName = phiConf.first('UserName') || confGED.first('UserName') || 'anonymous';
-// Read HPT areas:
-var encodingHPT = phiConf.last('EncodingHPT') || 'utf8';
-setup.areas = simteconf(phiConf.last('AreasHPT'), {
-   encoding: encodingHPT,
-   skipNames: ['#'],
-   lowercase: false,
-   prefixGroups: [
-      'NetmailArea',
-      'BadArea',
-      'DupeArea',
-      'LocalArea',
-      'EchoArea'
-   ]
-});
+   var simteconf = require('simteconf');
+   var phiConf = simteconf('phido.conf', {
+      skipNames: ['//', '#']
+   });
+   // Read GoldED settings:
+   var encodingGED = phiConf.last('EncodingGoldED') || 'utf8';
+   var confGED = simteconf(phiConf.last('ConfigGoldED'), {
+      encoding: encodingGED,
+      skipNames: ['//', '#', '-'+'-']
+   });
+   setup.UserName =
+      phiConf.first('UserName') || confGED.first('UserName') || 'anonymous';
+   // Read HPT areas:
+   var encodingHPT = phiConf.last('EncodingHPT') || 'utf8';
+   setup.areas = simteconf(phiConf.last('AreasHPT'), {
+      encoding: encodingHPT,
+      skipNames: ['#'],
+      lowercase: false,
+      prefixGroups: [
+         'NetmailArea',
+         'BadArea',
+         'DupeArea',
+         'LocalArea',
+         'EchoArea'
+      ]
+   });
 
 };
