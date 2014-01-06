00b731aa41717f44753b4172d6032539e74b0cba
Mithgol the Webmaster <getgit@mithgol.ru>
2014-01-06 11:23:27 +0400

    read `AreaSep` group from PhiDo's or GoldED's setup

--- script/setup.js
+++ script/setup.js
@@ -3,19 +3,28 @@
 setup = {};
 
 initSetup = function(){
-
    var simteconf = require('simteconf');
    var phiConf = simteconf('phido.conf', {
-      skipNames: ['//', '#']
+      skipNames: ['//', '#'],
+      prefixGroups: [
+         'AreaSep'
+      ]
    });
    // Read GoldED settings:
    var encodingGED = phiConf.last('EncodingGoldED') || 'utf8';
    var confGED = simteconf(phiConf.last('ConfigGoldED'), {
       encoding: encodingGED,
-      skipNames: ['//', '#', '-'+'-']
+      skipNames: ['//', '#', '-'+'-'],
+      prefixGroups: [
+         'AreaSep'
+      ]
    });
    setup.UserName =
       phiConf.first('UserName') || confGED.first('UserName') || 'anonymous';
+   setup.areaSeparators = phiConf.group('AreaSep');
+   if( setup.areaSeparators.names().length < 1 ){
+      setup.areaSeparators = confGED.group('AreaSep');
+   }
    // Read HPT areas:
    var encodingHPT = phiConf.last('EncodingHPT') || 'utf8';
    setup.areas = simteconf(phiConf.last('AreasHPT'), {
@@ -30,5 +39,4 @@ initSetup = function(){
          'EchoArea'
       ]
    });
-
 };
