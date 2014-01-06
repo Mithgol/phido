7589d60e82bf03e53becb682a57ec145c20f3458
Mithgol the Webmaster <getgit@mithgol.ru>
2014-01-07 00:45:11 +0400

    completed reading a filtered sorted list of areatag separators

--- script/setup.js
+++ script/setup.js
@@ -5,26 +5,35 @@ setup = {};
 initSetup = function(){
    var simteconf = require('simteconf');
    var phiConf = simteconf('phido.conf', {
-      skipNames: ['//', '#'],
-      prefixGroups: [
-         'AreaSep'
-      ]
+      skipNames: ['//', '#']
    });
    // Read GoldED settings:
    var encodingGED = phiConf.last('EncodingGoldED') || 'utf8';
    var confGED = simteconf(phiConf.last('ConfigGoldED'), {
       encoding: encodingGED,
-      skipNames: ['//', '#', '-'+'-'],
-      prefixGroups: [
-         'AreaSep'
-      ]
+      skipNames: ['//', '#', '-'+'-']
    });
    setup.UserName =
       phiConf.first('UserName') || confGED.first('UserName') || 'anonymous';
-   setup.areaSeparators = phiConf.group('AreaSep');
-   if( setup.areaSeparators.names().length < 1 ){
-      setup.areaSeparators = confGED.group('AreaSep');
-   }
+   setup.areaSeparators = (
+      phiConf.all('AreaSep') || confGED.all('AreaSep') || []
+   ).filter(function(areaSep){
+      var ok = /^\s*\S+\s+"[^"]+"\s+\d+\s+[Ee][Cc][Hh][Oo]\s*$/.test(areaSep);
+      return ok;
+   }).map(function(areaSep){
+      var matches =
+         /^\s*(\S+)\s+"([^"]+)"\s+\d+\s+[Ee][Cc][Hh][Oo]\s*$/.exec(areaSep);
+      return {
+         sepName: matches[1],
+         sepDesc: matches[2]
+      };
+   }).sort(function(a, b){
+      if( a.sepName < b.sepName ){
+         return -1;
+      } else if ( a.sepName > b.sepName ){
+         return 1;
+      } else return 0;
+   });
    // Read HPT areas:
    var encodingHPT = phiConf.last('EncodingHPT') || 'utf8';
    setup.areas = simteconf(phiConf.last('AreasHPT'), {
