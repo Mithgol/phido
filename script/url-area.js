104e11a41ba100594988ad946eff39a4ade18597
Mithgol the Webmaster <getgit@mithgol.ru>
2014-01-28 16:34:08 +0400

    do not (yet) attempt to open multiple areas

--- script/url-area.js
+++ script/url-area.js
@@ -1,5 +1,19 @@
-/* global renderAreaURL:true, arealist */
+/* global renderAreaURL:true, _, arealist, phiBar */
 
-renderAreaURL = function(/*URL, parsedURL*/){
-   arealist();
+renderAreaURL = function(URL, parsedURL){
+   console.log('Parsed URL:');
+   console.log(parsedURL);
+   if( parsedURL.echoNames.length < 1 ){
+      arealist();
+   } else if ( parsedURL.echoNames.length === 1 ){
+   } else {
+      phiBar.reportErrorHTML([
+         'Sorry, opening multiple echomail areas at once ',
+         'is not (yet) supported in PhiDo.',
+         '<p>The address <b>',
+         _.escapeHTML(URL),
+         '</b> could not be opened.',
+         '</p>'
+      ].join(''));
+   }
 };
